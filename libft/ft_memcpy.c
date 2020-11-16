8e618a34c1b63599c5159a0e5326757a912caade
Timour Petit <tpetit@shi-r9-p1.s19.be>
2020-11-16 15:41:14 +0100

    change user

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 14:52:57 by tpetit            #+#    #+#             */
-/*   Updated: 2020/11/16 15:22:35 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/11/16 15:40:27 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 void	*ft_memcpy(void *dst, void *src, size_t n)
 {
-	int 	i;
-	char	*new_dst;
-	char	*new_src;
-	char *test;
+	int 		i;
+	char		*new_dst;
+	char		*new_src;
 
 	new_dst = (char *)dst;
 	new_src = (char *)src;
-	test = (char *)dst;
 	i = -1;
 	while (++i < n)
 		new_dst[i] = new_src[i];
-	return (test);
+	return (dst);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int		main(int argc, char **argv)
 {
     char dst[100];
-    char src[100] = "hello comment ca va";
+    char src[100] = "comment ca va";
     printf("%s\n", ft_memcpy(dst, src, 5));
     printf("%s\n", dst);
 }
