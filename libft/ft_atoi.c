9fd572b7c4be72a24c9350418389ed34550207a6
TimourP <petit.timour@gmail.com>
2020-12-03 09:39:07 +0100

    all ok

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,13 +6,15 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 23:08:29 by tpetit            #+#    #+#             */
-/*   Updated: 2020/12/02 12:08:46 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/12/03 09:35:39 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_atoi(const char *str)
 {
-	unsigned long int	num;
+	unsigned long long	num;
 	int					neg;
 
 	neg = 1;
@@ -25,14 +27,16 @@ int	ft_atoi(const char *str)
 			neg *= -1;
 		str++;
 	}
+	if (ft_strlen(str) > 20)
+		return (neg == 1 ? -1 : 0);
 	while (*str >= '0' && *str <= '9')
 	{
 		num = 10 * num + (*str - '0');
 		str++;
 	}
-	if (num > 2147483647 && neg == 1)
+	if (num > 9223372036854775807 && neg == 1)
 		return (-1);
-	if (num > 2147483648 && neg == -1)
+	if (num > 9223372036854775807 && neg == -1)
 		return (0);
 	return (num * neg);
 }
