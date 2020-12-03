757c68544583d37e0f386d183698817e15797170
TimourP <petit.timour@gmail.com>
2020-12-03 09:40:47 +0100

    all ok

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,11 +6,11 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/09 21:47:18 by tpetit            #+#    #+#             */
-/*   Updated: 2020/12/01 14:23:31 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/12/03 09:40:10 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
