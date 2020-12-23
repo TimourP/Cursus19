3175bfb09cda4dd48ce668b25ef227b36561dc9b
timour petit <petit.timour@gmail.com>
2020-12-23 11:15:27 +0100

    gnl

---
+++ gnl/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/02 11:03:52 by tpetit            #+#    #+#             */
+/*   Updated: 2020/12/23 11:09:36 by tpetit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		main(void)
+{
+	int i;
+	int j;
+	char *li;
+	int value;
+
+	i = open("./test.txt", O_RDONLY);
+	j = open("./testbis.txt", O_RDONLY);
+	li = 0;
+
+	for (int u = 0; u < 50 ; u++)
+	{
+		value = get_next_line(i, &li);
+		printf("%d %d : %s\n", u, value, li);
+	}
+	close(i);
+	close(j);
+	return 0;
+}
