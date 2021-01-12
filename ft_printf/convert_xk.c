87a6a4df760f510fc88595723496179e34e7f796
TimourP <petit.timour@gmail.com>
2021-01-12 14:49:45 +0100

    end x xk

--- ft_printf/convert_xk.c
+++ ft_printf/convert_xk.c
@@ -6,16 +6,42 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 18:37:03 by tpetit            #+#    #+#             */
-/*   Updated: 2021/01/06 13:29:33 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/01/12 14:48:36 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int convert_X(ft_printf_data *print_variables)
+static int	convert_X_precision(ft_printf_data *print_variables)
 {
-	print_variables->current_str = ft_itoa_base_u(
-		va_arg(print_variables->argc, unsigned int), 16, "0123456789ABCDEF");
+	char	*space_string;
+	int		remaining_length;
+
+	remaining_length = print_variables->precision -
+		ft_strlen(print_variables->current_str);
+	if (!(space_string = string_with_length('0',
+		remaining_length > 0 ? remaining_length : 0)))
+		return (free_and_return(space_string, 0));
+	else if (!(print_variables->current_str =
+		join_front(print_variables->current_str, space_string)))
+		return (free_and_return(space_string, 0));
+	return (free_and_return(space_string, 1));
+}
+
+int			convert_X(ft_printf_data *print_variables)
+{
+	int nb;
+
+	nb = va_arg(print_variables->argc, unsigned int);
+	print_variables->current_str = ft_itoa_base_u(nb, 16, "0123456789ABCDEF");
+	if (print_variables->dot || print_variables->zero)
+	{
+		if (nb == 0)
+			print_variables->current_str =
+			ft_strdup(string_with_length('0', print_variables->precision));
+		else if (!(convert_X_precision(print_variables)))
+			return (0);
+	}
 	if (print_variables->min_length)
 		if (!convert_width(print_variables))
 			return (0);
