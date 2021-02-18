2a930ef3c6dbf8292ba99f79d824c548e83b53ac
TimourP <petit.timour@gmail.com>
2021-02-18 12:58:46 +0100

    edit .h

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 10:45:08 by tpetit            #+#    #+#             */
-/*   Updated: 2021/02/15 10:45:27 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/02/18 12:58:06 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,8 @@
 int		get_next_line(int fd, char **line);
 size_t	ft_strlen_until(const char *str, char c);
 char	*ft_strdup_until(char *src, char c);
-void	ft_strcpy_from(char *dst, char *src, char c);
 int		ft_is_in_str(char *str, char c);
 char	*ft_strjoin_until(char *s1, char *s2, char c);
-char	*ft_strcpy(char *dest, char *src);
 char	*ft_strdup_from(char *src, char c);
 
 #endif
