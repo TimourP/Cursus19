06073ed87c1914fb0fa0dd991a38d20182803f81
TimourP <petit.timour@gmail.com>
2021-03-08 13:58:04 +0100

    remove printf

--- ft_printf/ft_printf_utils2.c
+++ ft_printf/ft_printf_utils2.c
@@ -6,22 +6,11 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 11:47:10 by tpetit            #+#    #+#             */
-/*   Updated: 2021/03/08 13:42:42 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/03/08 13:57:45 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	print_struct(t_printf_data *pf_var)
-{
-	printf("curr str : %s\n", pf_var->current_str);
-	printf("curr char : %c\n", pf_var->current_char);
-	printf("minus : %d\n", pf_var->minus);
-	printf("width : %d\n", pf_var->width);
-	printf("zero : %d\n", pf_var->zero);
-	printf("dot : %d\n", pf_var->dot);
-	printf("curr precision : %d\n", pf_var->precision);
-}
 
 int		free_and_return(char *to_free, int ret)
 {
