a47586961598661dbb1ca15b2b0679d3e167d771
TimourP <petit.timour@gmail.com>
2021-04-19 13:30:23 +0200

    norme ok for now

--- cub3D/srcs/drawing/game.c
+++ cub3D/srcs/drawing/game.c
@@ -6,13 +6,41 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 19:56:48 by tpetit            #+#    #+#             */
-/*   Updated: 2021/04/19 12:27:08 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/04/19 13:29:59 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
 int	g_wall_color[4] = {COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_WHITE};
+
+static float	get_texture_value_end(t_ray *c_ray, t_ray_calc *calc)
+{
+	float	angle;
+	float	text_value;
+
+	if (get_side(calc) == 2)
+	{
+		angle = calc->angle - PI / 2;
+		text_value = sin(angle) * calc->final_dist
+			- (c_ray->player_posx - (int)c_ray->player_posx);
+		if (text_value < 0)
+			text_value = 1 - fabs(text_value - (int)text_value);
+		else
+			text_value = fabs(text_value - (int)text_value);
+	}
+	else if (get_side(calc) == 3)
+	{
+		angle = calc->angle + PI;
+		text_value = sin(angle) * calc->final_dist
+			- (c_ray->player_posy - (int)c_ray->player_posy);
+		if (text_value < 0)
+			text_value = 1 - fabs(text_value - (int)text_value);
+		else
+			text_value = fabs(text_value - (int)text_value);
+	}
+	return (text_value);
+}
 
 float	get_texture_value(t_ray *c_ray, t_ray_calc *calc)
 {
@@ -38,27 +66,21 @@ float	get_texture_value(t_ray *c_ray, t_ray_calc *calc)
 		else
 			text_value = fabs(text_value - (int)text_value);
 	}
-	else if (get_side(calc) == 2)
-	{
-		angle = calc->angle - PI / 2;
-		text_value = sin(angle) * calc->final_dist
-			- (c_ray->player_posx - (int)c_ray->player_posx);
-		if (text_value < 0)
-			text_value = 1 - fabs(text_value - (int)text_value);
-		else
-			text_value = fabs(text_value - (int)text_value);
-	}
-	else if (get_side(calc) == 3)
-	{
-		angle = calc->angle + PI;
-		text_value = sin(angle) * calc->final_dist
-			- (c_ray->player_posy - (int)c_ray->player_posy);
-		if (text_value < 0)
-			text_value = 1 - fabs(text_value - (int)text_value);
-		else
-			text_value = fabs(text_value - (int)text_value);
-	}
+	else
+		return (get_texture_value_end(c_ray, calc));
 	return (text_value);
+}
+
+static t_image	*text_side(t_ray *c_ray, int side)
+{
+	if (side == 0)
+		return (c_ray->c_map->north_t);
+	else if (side == 1)
+		return (c_ray->c_map->east_t);
+	else if (side == 2)
+		return (c_ray->c_map->south_t);
+	else
+		return (c_ray->c_map->west_t);
 }
 
 int	draw_game(t_ray *c_ray)
@@ -80,14 +102,8 @@ int	draw_game(t_ray *c_ray)
 		side = i_v[0];
 		i_v[1] = get_line_height(c_ray, (PI / FOV) / (c_ray->screen_w)
 				* (i_v[0] - c_ray->screen_w / 2), &side, &y_value);
-		if (!LIGHT && side == 0)
-			draw_vertical_texture(c_ray, i_v, c_ray->c_map->north_t, y_value);
-		else if (!LIGHT && side == 1)
-			draw_vertical_texture(c_ray, i_v, c_ray->c_map->east_t, y_value);
-		else if (!LIGHT && side == 2)
-			draw_vertical_texture(c_ray, i_v, c_ray->c_map->south_t, y_value);
-		else if (!LIGHT && side == 3)
-			draw_vertical_texture(c_ray, i_v, c_ray->c_map->west_t, y_value);
+		if (!LIGHT)
+			draw_vertical_texture(c_ray, i_v, text_side(c_ray, side), y_value);
 		else
 			draw_vertical_line(c_ray, i_v[0], i_v[1],
 				g_wall_color[side]);
--- cub3D/srcs/raycasting/distances.c
+++ cub3D/srcs/raycasting/distances.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 12:23:17 by tpetit            #+#    #+#             */
-/*   Updated: 2021/04/19 12:31:24 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/04/19 13:26:32 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,12 @@ int	get_line_height(t_ray *c_ray, float value, int *side, float *text_value)
 	calc.d_distx = fabs(div_zero(1, cos(calc.angle)));
 	calc.d_disty = fabs(div_zero(1, cos(PI / 2 - calc.angle)));
 	set_steps(c_ray, &calc);
-	set_hit(c_ray, &calc, 1);
+	set_hit(c_ray, &calc, 0);
 	*side = get_side(&calc);
 	calc.final_dist = calc.s_disty - calc.d_disty;
 	if (calc.final_dist < calc.s_distx - calc.d_distx)
 		calc.final_dist = calc.s_distx - calc.d_distx;
-	if (!LIGHT)
-		*text_value = get_texture_value(c_ray, &calc);
+	*text_value = get_texture_value(c_ray, &calc);
 	calc.line_height = c_ray->screen_h / calc.final_dist * 1.2;
 	calc.line_height = calc.line_height / cos(calc.angle - c_ray->player_angle);
 	if (calc.line_height > c_ray->screen_h * 3)
