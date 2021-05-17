e9552b02a9205f6e90cbce23985739401945cd4b
timour petit <petit.timour@gmail.com>
2021-05-17 18:27:53 +0200

    start swap and get groups size

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 14:08:27 by tpetit            #+#    #+#             */
-/*   Updated: 2021/05/14 16:03:33 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/05/17 18:24:55 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	is_in_int(int *list, int elem, int size)
 int main(void)
 {
 	int ra;
-	const int	nMax = 12;
+	const int	nMax = 20;
 	const int	nMin = 0;
-	const int	total = 12;
+	const int	total = 5;
 	int			*list;
 	int			i;
 
--- push_swap/push_swap_srcs/push_swap.c
+++ push_swap/push_swap_srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 13:33:17 by tpetit            #+#    #+#             */
-/*   Updated: 2021/05/14 15:55:46 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/05/17 18:24:05 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,68 @@ static int	get_median_value(t_stack *stack, int len)
 	return (num_list[len / 2]);
 }
 
+static int	*get_swap_groups(t_stack *a)
+{
+	int	*groups;
+	int	size;
+	int	stack_size;
+	int i;
+
+	size = 0;
+	stack_size = 0;
+	ft_stacksize(a, &stack_size);
+	while (stack_size > 2)
+	{
+		stack_size = stack_size - stack_size / 2;
+		size++;
+	}
+	groups = malloc(sizeof(int) * (size + 1));
+	i = -1;
+	stack_size = ft_stacksize(a, &stack_size);
+	while (++i < size)
+	{
+		groups[size - i - 1] = stack_size / 2;
+		stack_size = stack_size - groups[size - i - 1];
+	}
+	groups[i] = -1;
+	return (groups);
+}
+
+static void	solve_swap2(t_stack *a, t_stack *b, const int *groups)
+{
+	int	current_group;
+
+	current_group = 0;
+	if (a && a->next && a->content > a->next->content)
+		swap(&a);
+	if (groups[current_group] == 1)
+		push_a(&a, &b);
+	if (groups[current_group] == 2)
+	{
+		if (b->content < b->next->content)
+			swap(&b);
+		push_a(&a, &b);
+		push_a(&a, &b);
+	}
+	current_group--;
+	while (groups[++current_group] != -1)
+	{
+		printf("%d\n", groups[current_group]);
+	}
+	print_stack(a, b);
+}
+
 static void	solve_swap(t_stack *a)
 {
 	t_stack	*b;
 	int		size;
 	int		med;
 	int		count;
+	const int	*groups = get_swap_groups(a);
 
 	b = NULL;
 	size = 0;
+	get_swap_groups(a);
 	while (ft_stacksize(a, &size) > 2)
 	{
 		count = -1;
@@ -65,6 +118,7 @@ static void	solve_swap(t_stack *a)
 		size = 0;
 	}
 	print_stack(a, b);
+	solve_swap2(a, b, groups);
 }
 
 int main(int argc, char **argv)
