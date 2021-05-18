2290a20ddfa9ec7852b147da58c1cf672a96dc90
TimourP <petit.timour@gmail.com>
2021-05-18 17:28:04 +0200

    rm ft_server

--- ft_server/srcs/docker-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/Library/Containers/com.docker.docker
-rm -rf ~/.docker
-rm -rf /goinfre/${USER}/docker /goinfre/${USER}/agent
-mkdir -p /goinfre/${USER}/docker /goinfre/${USER}/agent
-ln -s /goinfre/${USER}/agent ~/Library/Containers/com.docker.docker
-ln -s /goinfre/${USER}/docker ~/.docker
