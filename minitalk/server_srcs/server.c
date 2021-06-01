6dfc272ec7a5e2735f7b402ce4ffaef1a22f943f
TimourP <petit.timour@gmail.com>
2021-06-01 11:27:07 +0200

    ok

--- minitalk/server_srcs/server.c
+++ minitalk/server_srcs/server.c
@@ -6,38 +6,13 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 12:06:20 by tpetit            #+#    #+#             */
-/*   Updated: 2021/06/01 10:29:36 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/06/01 11:05:19 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
 t_server *g_server;
-
-static char	*ft_join_one(char c)
-{
-	int i;
-	char *new_str;
-	int len;
-
-	if (!g_server->current_str)
-	{
-		new_str = malloc(sizeof(char) * 2);
-		new_str[0] = c;
-		new_str[1] = 0;
-		return (new_str);
-	}
-	i = -1;
-	len = ft_strlen(g_server->current_str);
-	new_str = malloc(sizeof(char) * (len + 2));
-	while (++i < len)
-		new_str[i] = g_server->current_str[i];
-	new_str[i] = c;
-	new_str[i + 1] = 0;
-	free(g_server->current_str);
-	g_server->current_str = NULL;
-	return (new_str);
-}
 
 void decode_binary(int signal)
 {
