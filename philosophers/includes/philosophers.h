76df60c5c983b59f9977ee3c462b275311d14f51
TimourP <petit.timour@gmail.com>
2021-06-07 19:37:34 +0200

    start init philo and think about struct

--- philosophers/includes/philosophers.h
+++ philosophers/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 17:59:05 by tpetit            #+#    #+#             */
-/*   Updated: 2021/06/07 18:36:58 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/06/07 18:57:04 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,20 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <sys/time.h>
+# include<pthread.h>
 
 # define MALLOC_ERROR 0
 # define NBR_ARGS_ERROR 1
 # define ARGS_ERROR 2
+
+typedef struct s_philo
+{
+	int	id;
+	int	nbr_philo;
+	int	time_die;
+	int	time_eat;
+	int	time_sleep;
+}				t_philo;
 
 /*
 **	UTILS
--- philosophers/phi1_srcs/main.c
+++ philosophers/phi1_srcs/main.c
@@ -6,11 +6,33 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:04:27 by tpetit            #+#    #+#             */
-/*   Updated: 2021/06/07 18:31:45 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/06/07 19:37:07 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
+
+static void	init_philosophers(int argc, char **argv)
+{
+	int			ndest[5];
+	int			i;
+	pthread_t	p_id;
+
+	ndest[0] = phi_atoi(argv[1]);
+	ndest[1] = phi_atoi(argv[2]);
+	ndest[2] = phi_atoi(argv[3]);
+	ndest[3] = phi_atoi(argv[4]);
+	if (argc == 6)
+		ndest[4] = phi_atoi(argv[5]);
+	else
+		ndest[4] = 0;
+	i = -1;
+	p_id = malloc(sizeof(pthread_t));
+	while (++i < ndest[0])
+	{
+		/* code */
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -18,5 +40,6 @@ int	main(int argc, char **argv)
 		exit_message(NBR_ARGS_ERROR);
 	if (!check_argv(argc, argv))
 		exit_message(ARGS_ERROR);
+	init_philosophers(argc, argv);
 	return (0);
 }
