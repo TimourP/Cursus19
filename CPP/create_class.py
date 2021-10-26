4ac3f3729a16da6339e930348b9514c24077f3cc
TimourP <petit.timour@gmail.com>
2021-10-26 14:18:29 +0200

    end create class

--- CPP/create_class.py
+++ CPP/create_class.py
@@ -1,11 +1,24 @@
 import os
 import sys
+import datetime
 
 special_char_1 = "{"
 special_char_2 = "}"
 
 def return_class_h_string(class_name):
-	stri = f"""
+	d = datetime.datetime.now()
+	stri = f"""/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: {d.strftime("%Y/%m/%d %H:%M:%S")} by tpetit            #+#    #+#             */
+/*   Updated: 2021/10/26 14:05:03 by tpetit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef {class_name.upper()}_H
 # define {class_name.upper()}_H
 
@@ -25,6 +38,29 @@ class {class_name} {special_char_1}
 	"""
 	return stri
 
+
+def return_class_cpp_string(class_name):
+	d = datetime.datetime.now()
+	stri = f"""/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: {d.strftime("%Y/%m/%d %H:%M:%S")} by tpetit            #+#    #+#             */
+/*   Updated: 2021/10/26 14:05:03 by tpetit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "{class_name}.hpp"
+
+{class_name}::{class_name}( void ) {special_char_1}{special_char_2}
+
+{class_name}::~{class_name}( void ) {special_char_1}{special_char_2}
+	"""
+	return stri
+
 pwd = os.getcwd()
 n = len(sys.argv)
 
@@ -32,6 +68,11 @@ if n != 2:
 	print("Please enter the name of the class")
 	exit(0)
 
+print()
+
 
 with open(sys.argv[1] + ".hpp", "w") as f:
 	f.write(return_class_h_string(sys.argv[1]))
+
+with open(sys.argv[1] + ".cpp", "w") as f:
+	f.write(return_class_cpp_string(sys.argv[1]))
--- CPP/module_01/ex04/Replace.hpp
+++ CPP/module_01/ex04/Replace.hpp
@@ -1,16 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/26 08:44:53 by tpetit            #+#    #+#             */
-/*   Updated: 2021/10/26 14:05:03 by tpetit           ###   ########.fr       */
+/*   Created: 2021/10/26 14:15:02 by tpetit            #+#    #+#             */
+/*   Updated: 2021/10/26 14:15:07 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
-{
-	return (0);
-}
+#ifndef REPLACE_H
+# define REPLACE_H
+
+# include "string"
+# include "iostream"
+
+class Replace {
+	public:
+		Replace( void );
+		~Replace( void );
+	
+	private:
+
+};
+
+#endif // REPLACE_H
+	
