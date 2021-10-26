ec2dcf35d058c17f961ecb1d7c2569119520d586
TimourP <petit.timour@gmail.com>
2021-10-26 15:01:51 +0200

    commit to not lose

---
+++ CPP/module_01/ex05/Karen.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 15:00:27 by tpetit            #+#    #+#             */
+/*   Updated: 2021/10/26 15:00:33 by tpetit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KAREN_H
+# define KAREN_H
+
+# include "string"
+# include "iostream"
+
+class Karen {
+	public:
+		Karen( void );
+		~Karen( void );
+	
+	private:
+
+};
+
+#endif // KAREN_H
+	
