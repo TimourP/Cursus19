6c1e68231e2eccde4e9b7253541ee83156685c7e
TimourP <petit.timour@gmail.com>
2022-02-28 11:22:21 +0100

    start correcting cpp module 01

--- CPP/module_01/ex00/Zombie.cpp
+++ CPP/module_01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 08:44:53 by tpetit            #+#    #+#             */
-/*   Updated: 2021/10/26 09:43:23 by tpetit           ###   ########.fr       */
+/*   Updated: 2022/02/28 11:12:07 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ Zombie::~Zombie( void )
 
 void Zombie::annonce( void ) const
 {
-	std::cout << this->_name << " BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
 void Zombie::name_zombie( std::string name )
--- CPP/module_01/ex02/main.cpp
+++ CPP/module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 09:48:12 by tpetit            #+#    #+#             */
-/*   Updated: 2021/10/26 10:00:45 by tpetit           ###   ########.fr       */
+/*   Updated: 2022/02/28 11:14:00 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,11 @@ int main( void )
 	std::string*	stringPTR = &str;
 	std::string&	stringREF = str;
 
+	std::cout << "Here is the string address: " << &str << std::endl;
+	std::cout << "Here is the address stocked in the pointer: " << stringPTR << std::endl;
+	std::cout << "Here is the address stocked in the referencd: " << &stringREF << std::endl;
+
 	std::cout << "Here is the string: " << str << std::endl;
 	std::cout << "Here is the value of the dereferenced pointer: " << *stringPTR << std::endl;
 	std::cout << "Here is the value of the reference: " << stringREF << std::endl;
-
-	std::cout << "You can edit value of the string directly by assigning a new value directly to that string: str = \"Hello World!\";" << std::endl;
-	str = "Hello World!";
-	std::cout << "New value of str: " << str << std::endl;
-
-	std::cout << "You can change the value through the pointer: *stringPTR = \"What a cliché!\";" << std::endl;
-	*stringPTR = "What a cliché!";
-	std::cout << "New value of str: " << str << std::endl;
-
-	std::cout << "You can change the value through the reference: stringREF = \"I am done with this test.\";" << std::endl;
-	stringREF = "I am done with this test.";
-	std::cout << "New value of str: " << str << std::endl;
 }
