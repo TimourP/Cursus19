aa9bea28d9d978d2674b6255fc56ce614d054568
TimourP <petit.timour@gmail.com>
2022-04-11 09:53:28 +0200

    some comments

--- inception/srcs/requirements/mariadb/tools/config.sh
+++ inception/srcs/requirements/mariadb/tools/config.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-# start mysql service
-
+# setup mysql config file in order to acces it remotely
 sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# start mysql service
 service mysql start
 
+# sleep to be sure service is on (it is just because i'm paranoid)
 sleep 2
 
+# use -e flag in order to pass param
+# create database with database_name if not exist and grant privileges to database user
+# it will create user if not exist and identified it with password
 mysql -e "\
 CREATE DATABASE IF NOT EXISTS $WP_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
 GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USR'@'%.%.%.%' IDENTIFIED BY 'secret';
 FLUSH PRIVILEGES;"
 
-
+# sleep in order to allow me to open shell inside of this container
 sleep infinity
