3ea009ff713161f2ddd874548a444384e77815df
TimourP <petit.timour@gmail.com>
2022-04-13 11:16:54 +0200

    config for site url

--- inception/srcs/requirements/wordpress/conf/wp-config.php
+++ inception/srcs/requirements/wordpress/conf/wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define( 'WP_HOME', 'https://tpetit.42.fr' );
+define( 'WP_SITEURL', 'https://tpetit.42.fr' );
+
 /**#@+
  * Authentication unique keys and salts.
  *
