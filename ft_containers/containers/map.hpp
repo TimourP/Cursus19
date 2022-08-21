5f8cadb1523730ef41b2227634d4a5f228f356a9
timourp <petit.timour@gmail.com>
2022-08-21 09:07:51 +0200

    feat: declare all needed member functions

--- ft_containers/containers/map.hpp
+++ ft_containers/containers/map.hpp
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 15:35:18 by tpetit            #+#    #+#             */
-/*   Updated: 2022/08/20 19:35:45 by tpetit           ###   ########.fr       */
+/*   Updated: 2022/08/21 09:06:03 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,8 @@ namespace ft
 			typedef int											difference_type;
 			typedef size_t										size_type;
 
+			// Constructors
+			
 			explicit map (const key_compare& comp = key_compare(),
 			const allocator_type& alloc = allocator_type()) :tree(comp), _alloc(alloc), _size(0) {
 				
@@ -58,7 +60,16 @@ namespace ft
 
 			map (const map& x);
 
+			// Destructor
 			~map() {};
+			
+			map& operator=( const map& other );
+			
+			mapped_type& at( const Key& key );
+			
+			const mapped_type& at( const Key& key ) const;
+			
+			mapped_type& operator[]( const Key& key );
 			
 			iterator begin(void) {
 				if (empty())
@@ -66,10 +77,22 @@ namespace ft
 				return iterator(RBTNode<key_type, mapped_type, value_compare>::get_smallest(tree.getRoot()));
 			}
 			
+			const_iterator begin() const;
+			
 			iterator end(void) {
 				return iterator(tree.getEnd());
 			}
-
+			
+			const_iterator end() const;
+			
+			reverse_iterator rbegin();
+			
+			const_reverse_iterator rbegin() const;
+			
+			reverse_iterator rend();
+			
+			const_reverse_iterator rend() const;
+			
 			bool empty() const {
 				return this->tree.empty();
 			};
@@ -78,54 +101,53 @@ namespace ft
 			
 			size_type max_size() const;
 
-
-			mapped_type& operator[] (const key_type& k);
-
-
-			/*pair<iterator,bool>*/ void insert (const value_type& val) {
+			pair<iterator,bool> void insert (const value_type& val) {
 				this->tree.insert(val);
 			};
+			
+			iterator insert( iterator hint, const value_type& value );
 
-			//iterator insert (iterator position, const value_type& val);
+			template <class InputIterator>
+			void insert (InputIterator first, InputIterator last);
 
-			// template <class InputIterator>
-			// void insert (InputIterator first, InputIterator last);
-
-
-			// void erase (iterator position);
+			void erase (iterator position);
 
 			size_type erase (const key_type& k);
 
-			// void erase (iterator first, iterator last);
-
+			void erase (iterator first, iterator last);
 
 			void swap (map& x);
 
-
 			void clear();
-
 
 			allocator_type get_allocator() const;
 
 
-			// pair<const_iterator,const_iterator> equal_range (const key_type& k) const;
+			pair<const_iterator,const_iterator> equal_range (const key_type& k) const;
 
-			// pair<iterator,iterator>             equal_range (const key_type& k);
-
+			pair<iterator,iterator>             equal_range (const key_type& k);
 
 			size_type count (const key_type& k) const;
 
+			iterator find( const Key& key );
+			
+			const_iterator find( const Key& key ) const;
+			
+			iterator lower_bound( const Key& key );
+			
+			const_iterator lower_bound( const Key& key ) const;
+			
+			iterator upper_bound( const Key& key );
+			
+			const_iterator upper_bound( const Key& key ) const;
 
 			value_compare value_comp() const;
 
-
 			key_compare key_comp() const;
-			
-			ft::RBTree<key_type, mapped_type, value_compare, Allocator> tree;
 
 		private:
 			
-
+			ft::RBTree<key_type, mapped_type, value_compare, Allocator> tree;
 			allocator_type _alloc;
 			size_type _size;
 			size_type _max_size;
