1dc255a61af849f901f66f62adbec69e6917361e
TimourP <petit.timour@gmail.com>
2022-10-31 14:11:12 +0100

    refactor: mysqltest

--- inception/srcs/requirements/wordpress/tools/config.sh
+++ inception/srcs/requirements/wordpress/tools/config.sh
@@ -2,17 +2,18 @@
 
 sleep 2
 
-adduser nginx
-echo "nginx:$WP_DATABASE_PWD" | chpasswd
-
-mkdir /run/php
-
-sed -i "s/define( 'DB_NAME', '' );/define( 'DB_NAME', '$WP_DATABASE_NAME' );/" /tmp/wp-config.php
-sed -i "s/define( 'DB_USER', '' );/define( 'DB_USER', '$WP_DATABASE_USR' );/" /tmp/wp-config.php
-sed -i "s/define( 'DB_PASSWORD', '' );/define( 'DB_PASSWORD', '$WP_DATABASE_PWD' );/" /tmp/wp-config.php
-sed -i "s/define( 'DB_HOST', '' );/define( 'DB_HOST', '$MYSQL_HOST' );/" /tmp/wp-config.php
 
 if ! wp core is-installed --allow-root; then
+
+	adduser nginx
+	echo "nginx:$WP_DATABASE_PWD" | chpasswd
+
+	mkdir /run/php
+
+	sed -i "s/define( 'DB_NAME', '' );/define( 'DB_NAME', '$WP_DATABASE_NAME' );/" /tmp/wp-config.php
+	sed -i "s/define( 'DB_USER', '' );/define( 'DB_USER', '$WP_DATABASE_USR' );/" /tmp/wp-config.php
+	sed -i "s/define( 'DB_PASSWORD', '' );/define( 'DB_PASSWORD', '$WP_DATABASE_PWD' );/" /tmp/wp-config.php
+	sed -i "s/define( 'DB_HOST', '' );/define( 'DB_HOST', '$MYSQL_HOST' );/" /tmp/wp-config.php
 
 	mv /tmp/index.html /var/www/html/index.html
 
