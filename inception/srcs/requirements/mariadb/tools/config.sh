6bdcdfb6f4cbc1618f070ee9594f74e506abcaa7
TimourP <petit.timour@gmail.com>
2022-10-31 13:58:32 +0100

    refactor: mysqltest

--- inception/srcs/requirements/mariadb/tools/config.sh
+++ inception/srcs/requirements/mariadb/tools/config.sh
@@ -1,18 +1,14 @@
-# Give owner and group for the the DB which is normally automatically created in this folder 
+
 chown -R mysql:mysql /var/lib/mysql
 
-sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-#Just checking if the DB has been correctly created in the right path
 if [ ! -d /var/lib/mysql/$WP_DATABASE_NAME ]; then
-	service mysql start --datadir=/var/lib/mysql
-	echo "--Service started"
 
-	# Create a folder for the daemon (mysql server’s socket file)
+    sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+    sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+	service mysql start --datadir=/var/lib/mysql
+
 	mkdir -p /var/run/mysqld
-	# Setting up .pid if it's not automatically set 
-	# .sock file has been automatically created at this point
 	touch /var/run/mysqld/mysqlf.pid
 
 	mysql -e "\
@@ -21,22 +17,15 @@ if [ ! -d /var/lib/mysql/$WP_DATABASE_NAME ]; then
     GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO $WP_DATABASE_USR;
     FLUSH PRIVILEGES;"
 
-	# Set MySQL root password (if you don't set it no password at all)
-	mysqladmin -u root password $WP_DATABASE_PWD;
-	echo "--Password set"
+    # set root password
+	mysqladmin -u root password $MYSQL_ROOT_PWD;
 
 	service mysql stop --datadir=/var/lib/mysql
-	echo "--Service stopped"
 else
-	# Create a folder for the daemon (mysql server’s socket file)
 	mkdir -p /var/run/mysqld
-	#Setting up .pid and .sock since they're not automatically set
 	touch /var/run/mysqld/mysqlf.pid
 	mkfifo /var/run/mk
 fi
 
-#Give owner and group to that too
 chown -R mysql:mysql /var/run/mysqld
-#Start the database daemon
-echo "--start DB daemon"
 mysqld_safe --datadir=/var/lib/mysql
