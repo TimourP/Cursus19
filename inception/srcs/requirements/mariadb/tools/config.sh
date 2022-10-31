6bca8cf0affc1308cda48de26cce93a9fbbba59a
TimourP <petit.timour@gmail.com>
2022-10-31 13:46:22 +0100

    refactor: mysqltest

--- inception/srcs/requirements/mariadb/tools/config.sh
+++ inception/srcs/requirements/mariadb/tools/config.sh
@@ -1,32 +1,39 @@
-#!/bin/sh
+# Give owner and group for the the DB which is normally automatically created in this folder 
 chown -R mysql:mysql /var/lib/mysql
-# setup mysql config file in order to acces it remotely
+
+#Just checking if the DB has been correctly created in the right path
 if [ ! -d /var/lib/mysql/$WP_DATABASE_NAME ]; then
-    sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-    sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+	service mysql start --datadir=/var/lib/mysql
+	echo "--Service started"
 
-    # start mysql service
-    service mysql start
-    # sleep to be sure service is on (it is just because i'm paranoid)
-    sleep 2
+	# Create a folder for the daemon (mysql server’s socket file)
+	mkdir -p /var/run/mysqld
+	# Setting up .pid if it's not automatically set 
+	# .sock file has been automatically created at this point
+	touch /var/run/mysqld/mysqlf.pid
 
-    # use -e flag in order to pass param
-    # create database with database_name if not exist and grant privileges to database user
-    # it will create user if not exist and identified it with password
-    mysql -e "\
+	mysql -e "\
     CREATE DATABASE IF NOT EXISTS $WP_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
     CREATE USER IF NOT EXISTS $WP_DATABASE_USR IDENTIFIED BY '$WP_DATABASE_PWD';
     GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO $WP_DATABASE_USR;
     FLUSH PRIVILEGES;"
-    mysqladmin -u root password $WP_DATABASE_PWD
+
+	# Set MySQL root password (if you don't set it no password at all)
+	mysqladmin -u root password $WP_DATABASE_PWD;
+	echo "--Password set"
+
+	service mysql stop --datadir=/var/lib/mysql
+	echo "--Service stopped"
 else
 	# Create a folder for the daemon (mysql server’s socket file)
-    echo "je passe ici"
+	mkdir -p /var/run/mysqld
 	#Setting up .pid and .sock since they're not automatically set
-	# service mysql start
+	touch /var/run/mysqld/mysqlf.pid
+	mkfifo /var/run/mk
 fi
-# sleep in order to allow me to open shell inside of this container
 
-# mysql --user=$WP_DATABASE_USR --password=$WP_DATABASE_PWD
-
-sleep infinity
+#Give owner and group to that too
+chown -R mysql:mysql /var/run/mysqld
+#Start the database daemon
+echo "--start DB daemon"
+mysqld_safe --datadir=/var/lib/mysql
