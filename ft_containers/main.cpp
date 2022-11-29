132f75492ee00f51ea42a494d37b7204a29d976b
TimourP <petit.timour@gmail.com>
2022-11-29 14:00:09 +0100

    feat: beautiful print

--- ft_containers/main.cpp
+++ ft_containers/main.cpp
@@ -6,16 +6,26 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 13:55:34 by tpetit            #+#    #+#             */
-/*   Updated: 2022/11/29 11:34:54 by tpetit           ###   ########.fr       */
+/*   Updated: 2022/11/29 13:57:49 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests/map_tests.hpp"
+#include "tests/tests.hpp"
 
 int main(void) {
 
 
+	// vector tests
+	vector_test_print();
+	//map_tests();
+
+	// stack tests
+	stack_test_print();
+	//map_tests();
+
 	// map tests
+	map_test_print();
 	map_tests();
 
 	return 0;
--- ft_containers/tests/tests.hpp
+++ ft_containers/tests/tests.hpp
@@ -2,8 +2,53 @@
 #ifndef TESTS_H
 #define TESTS_H
 
+#include <iostream>
+#include <istream>
+#include <fcntl.h>
+#include <fstream>
+#include <sstream>
 
-template <class TYPE>
-void print_ft_std(FT<TYPE> ft_container, );
+#define RST  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+
+void map_test_print(void) {
+	std::ifstream infile("./tests/map_tests.txt");
+	std::stringstream ss;
+	std::string str_resp;
+
+	ss << infile.rdbuf();
+	std::cout << std::endl << std::endl << std::endl;
+	std::cout << KMAG << ss.str() << RST << std::endl;
+	std::cout << std::endl << std::endl << std::endl;
+}
+
+void vector_test_print(void) {
+	std::ifstream infile("./tests/vector_tests.txt");
+	std::stringstream ss;
+	std::string str_resp;
+
+	ss << infile.rdbuf();
+	std::cout << std::endl << std::endl << std::endl;
+	std::cout << KMAG << ss.str() << RST << std::endl;
+	std::cout << std::endl << std::endl << std::endl;
+}
+
+void stack_test_print(void) {
+	std::ifstream infile("./tests/stack_tests.txt");
+	std::stringstream ss;
+	std::string str_resp;
+
+	ss << infile.rdbuf();
+	std::cout << std::endl << std::endl << std::endl;
+	std::cout << KMAG << ss.str() << RST << std::endl;
+	std::cout << std::endl << std::endl << std::endl;
+}
 
 #endif
